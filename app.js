d952236599585e5d56bda17e63460b4d72d64633
Wesley Francis <wesley.allright@gmail.com>
2024-11-25 11:46:10 -0300

    Alterado texto para plural

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('h1', 'Jogo do número secretos');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
